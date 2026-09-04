4cd430fd822e805bd277e1d2bcc9eeab1013142c
João Abreu <joaoabreu@192.168.0.11>
2026-09-04 14:50:24 -0300

    atualizações no menu

--- menu.c
+++ menu.c
@@ -7,20 +7,55 @@ typedef struct{
     int cpf;
     int disciplinas[10];
     char genero;
+    char aluno_ou_professor;
 
 }Pessoa;
 
 int main(){
     int opcao;
-    puts("PROJETO ESCOLA\n\n");
+    int sair=0;
 
-    puts("Escolha uma opção:");
+    while(sair==0){
+        puts("PROJETO ESCOLA\n\n");
 
-    puts("\t0 - Sair");
-    puts("\t1 - Cadastrar");
-    puts("\t2 - Alterar");
-    puts("\t3 - Excluir");
-    puts("\t4 - Listar");
-    scanf("%d", &opcao);
+        puts("Escolha uma opção:");
+
+        puts("\t0 - Sair");
+        puts("\t1 - Cadastrar");
+        puts("\t2 - Alterar");
+        puts("\t3 - Excluir");
+        puts("\t4 - Listar");
+
+        scanf("%d", &opcao);
+        getchar();
+        switch(opcao){
+            case 0:{
+                sair=1;
+                break;
+            }
+            case 1:{
+                puts("Cadastrar:\n");
+                puts("\t1 - Aluno:\n");
+                puts("\t2 - Professor:\n");
+                puts("\t2 - Disciplina:\n");
+                getchar();
+                break;
+            }
+            case 2:{
+
+                break;
+            }
+            case 3:{
+
+                break;
+            }
+            case 4:{
+                break;
+            }
+            default:{
+                break;
+            }
+        }
+    }
     return 0;
 }
